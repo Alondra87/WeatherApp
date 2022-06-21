936e5e5ea53870ffc4785d9a9ba62124f3d00756
Alondra87 <aa5887687@gmail.com>
2022-06-21 18:48:23 -0400

    Adding functionalities

---
+++ dist/js/domFunctions.js
@@ -0,0 +1,23 @@
+export const addSpinner = (element) => {
+    animateButton(element);
+    animateButton(animateButton, 1000, element);
+}
+
+const animateButton = (element) => {
+    element.classList.toggle("none");
+    element.nextElementSibling.classList.toggle("block");
+    element.nextElementSibling.classList.toggle("none");
+};
+
+/*const geoError = (errObj) => {
+    const errMsg = errObj.message ? errObj.message : "Geolocation not supported";
+    displayError(errMsg, errMsg);
+};*/
+
+export const displayError = (headerMsg, srMsg) => {
+    updateWeatherLocationHeader(headerMsg);
+    updateScreenReaderConfirmation(srMsg);
+};
+const updateWeatherLocationHeader = (message) => {
+    const h1 = document
+}
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,26 @@
+import {
+    addSpinner
+} from "./domFunctions.js";
+import CurrentLocation from "./CurrentLocation.js";
+const currentLoc = new CurrentLocation();
 const initApp = () => {
+    // add listeners
+    const geoButton = document.getElementById("getLocation");
+    geoButton.addEventListener("click", getGeoWeather);
+    // set up
+    // load weather
+}
 
+document.addEventListener("DOMContentLoaded", initApp);
+
+const getGeoWeather = (event) => {
+    if (event) {
+        if (event.type === "click") {
+            //add spinner
+            const mapIcon = document.querySelector(".fa-map-marker-alt");
+            addSpinner(mapIcon);
+        }
+    }
+    /* if (!navigator.geolocation) geoError();
+     navigator.geolocation.getCurrentPosition(geoSuccess, geoError); */
 }
